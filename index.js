5e44f5ec189ef6922f1dfd0f61fab9e8a04726df
reszegiroland01 <reszegiroland01@gmail.com>
2024-09-04 15:11:34 +0200

    costumer feedback

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ function renderCategories(categoriesArray) {
     });
     document.getElementById("category-container").innerHTML = itemsHTML; 
 }
+
+function getStandOutDishes()
+{
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+    .then(dishes => dishes.json())
+    .then(dishesY => renderDishes(dishesY.meals) )
+}
+getStandOutDishes()
+
+function renderDishes(dishesArray){
+    let itemsHTML = "";
+    dishesArray.forEach((dish)=>{
+        
+        itemsHTML += '<div class="standout-dish-container">'
+        itemsHTML += '<div class="standout-dish-img-container"> <img class="standout-dish-img" src="' +dish.strMealThumb+ '" alt=""> </div>'
+        itemsHTML += '<p class="standout-dish-title category-title">'+dish.strMeal+'</p>'
+        itemsHTML += '<p class="standout-dish-sub-title category-sub-title">'+dish.strTags+'</p>'
+
+        itemsHTML += '<div class="standout-dish-thumb">'
+        itemsHTML += '<div class="standout-dish-sub-price">'+dish.idMeal+' Ft</div>'
+        itemsHTML += '<div class="standout-dish-sub-rating">'+dish.idMeal/15000+'</div>'
+        itemsHTML += '</div>'
+        itemsHTML += '</div>'
+        
+    })
+
+    document.getElementById("standout-dishes-container").innerHTML = itemsHTML
+}
